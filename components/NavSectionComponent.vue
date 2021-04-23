4baaf659ca301619eb2833545e0a5ee2b96dd6f1
Lino09 <onosmx@gmail.com>
2021-04-23 12:28:04 -0500

    navfix6

--- components/NavSectionComponent.vue
+++ components/NavSectionComponent.vue
@@ -38,43 +38,43 @@
                         </a>
 
                         <a href="#" v-scroll-to="'#services-section'"
-                           class="text-base font-medium text-gray-500 hover:text-gray-900 flex flex-nowrap">
+                           class="text-base font-medium text-gray-500 hover:text-gray-900 flex flex-nowrap h-8 items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                  stroke="currentColor" class="w-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                       d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                             </svg>
-                            Servicios
+                           <span> Servicios</span>
                         </a>
 
 
-                        <a href="#" v-scroll-to="'#projects-section'" class="text-base font-medium text-gray-500 hover:text-gray-900 flex">
+                        <a href="#" v-scroll-to="'#projects-section'" class="text-base font-medium text-gray-500 hover:text-gray-900 flex flex-nowrap h-8 items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                  stroke="currentColor" class="w-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                       d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
                             </svg>
-                            Proyectos
+                           <span> Proyectos</span>
                         </a>
 
                         <a href="#" v-scroll-to="'#team-section'"
-                           class="text-base font-medium text-gray-500 hover:text-gray-900 flex">
+                           class="text-base font-medium text-gray-500 hover:text-gray-900 flex flex-nowrap h-8 items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                  stroke="currentColor" class="w-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                       d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
                             </svg>
-                            Nosotros
+                           <span> Nosotros</span>
                         </a>
 
                         <a href="#" v-scroll-to="'#contact-section'"
-                           class="text-base font-medium text-gray-500 hover:text-gray-900 flex">
+                           class="text-base font-medium text-gray-500 hover:text-gray-900 flex flex-nowrap h-8 items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                  stroke="currentColor" class="w-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
                             </svg>
-                            Contacto
+                           <span> Contacto</span>
                         </a>
                     </nav>
                     <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
