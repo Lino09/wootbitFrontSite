f4f4ca29b4451f127e3f35b4707b89154cf51dae
Lino09 <onosmx@gmail.com>
2021-04-26 14:36:59 -0500

    logo fix and /login page

--- components/NavSectionComponent.vue
+++ components/NavSectionComponent.vue
@@ -5,15 +5,15 @@
                 <div class="flex justify-end items-center max-w-7xl
                             mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
 
-                    <!-- <div class="flex justify-start flex-none">
+                    <div class="flex justify-center md:justify-start flex-none md:w-auto w-full place-self-center">
                         <a href="#">
                             <span class="sr-only">logo</span>
                             <img class="hidden lg:block lg:md-12 h-12"
                                  src="../static/letrasWtbt.png" alt="">
-                            <img class="lg:hidden w-12"
+                            <img class="lg:hidden w-16 md:w-12 ml-10 md:ml-0  "
                                  src="../static/wootbitiologo.png" alt="">
                         </a>
-                    </div> -->
+                    </div>
                     <div class="-mr-2 -my-2 md:hidden">
                         <button type="button"
                                 class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
@@ -29,15 +29,6 @@
                     </div>
 
                     <nav class="hidden md:flex w-full space-x-10 items-center justify-center">
-<div class="flex justify-start flex-none">
-                        <a href="#">
-                            <span class="sr-only">logo</span>
-                            <img class="hidden lg:block lg:md-12 h-12"
-                                 src="../static/letrasWtbt.png" alt="">
-                            <img class="lg:hidden w-12"
-                                 src="../static/wootbitiologo.png" alt="">
-                        </a>
-                    </div>
                         <a href="#" class="font-medium text-gray-500 hover:text-gray-900 flex flex-nowrap h-8 items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                  stroke="currentColor" class="w-5">
@@ -87,15 +78,17 @@
                            <span> Contacto</span>
                         </a>
                     </nav>
-                    <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                    <div class="hidden md:flex items-center justify-end md:flex-1">
                         <!--                        <a href="#"-->
                         <!--                           class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">-->
                         <!--                            Sign in-->
                         <!--                        </a>-->
-                        <!--                        <a href="#"-->
-                        <!--                           class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">-->
-                        <!--                            Sign up-->
-                        <!--                        </a>-->
+                    <a href="/login">
+                      <button class="hidden  ml-8 whitespace-nowrap lg:inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">Sign In</button>
+                      <svg class="text-indigo-600 w-6 h-6 lg:hidden" xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                     </a>
                     </div>
                 </div>
 
@@ -123,8 +116,8 @@
                             <div class="pt-2 pb-6 px-4">
                                 <div class="flex items-center justify-between">
                                     <div>
-                                        <img class="h-8 w-auto"
-                                             src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                        <img class="h-16 w-auto"
+                                             src="../static/wootbitiologo.png"
                                              alt="Workflow">
                                     </div>
                                     <div class="-mr-2">
@@ -207,8 +200,8 @@
                                     </nav>
                                 </div>
                             </div>
-                            <div class="hidden py-6 px-5">
-                                <div class="grid grid-cols-2 gap-4">
+                            <div class="py-6 px-5">
+                                <div class=" hidden grid-cols-2 gap-4">
                                     <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
                                         Pricing
                                     </a>
@@ -221,17 +214,17 @@
                                         Company
                                     </a>
                                 </div>
-                                <div class="mt-6">
-                                    <a href="#"
+                                <div class="">
+                                    <a href="/login"
                                        class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                                        Sign up
+                                        Sign in
                                     </a>
-                                    <p class="mt-6 text-center text-base font-medium text-gray-500">
+                                    <!-- <p class="mt-6 text-center text-base font-medium text-gray-500">
                                         Existing customer?
                                         <a href="#" class="text-gray-900">
                                             Sign in
                                         </a>
-                                    </p>
+                                    </p> -->
                                 </div>
                             </div>
                         </div>
--- nuxt.config.js
+++ nuxt.config.js
@@ -68,5 +68,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+   
+  },
 }
